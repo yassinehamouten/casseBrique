c8d24e5f0a7cff3d5c92d60d46bd2b27a999adc3
Gilles <gilux762@gmail.com>
2016-03-31 09:08:24 +0200

    Affichage des briques

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ $(document).on('ready',function(){
 	
 	var longBrique = 50; // Longueur d'une brique
 	var hautBrique = 30; // Hauteur d'une brique
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
 	
 	var listeBriques = [
-	[1,1],
-	[1,2],
-	[1,4],
-	[1,5],
-	[2,1]
+	[1,1,'#000000'],
+	[1,2,'#13DD32'],
+	[1,4,'#93B0D3'],
+	[1,5,'#2201BC'],
+	[2,1,'#1CDDFF']
 	];
 	
 	drawLevel();
@@ -37,7 +39,10 @@ $(document).on('ready',function(){
 	{
 		for(i in listeBriques)
 		{
-			console.log('x = ' + (listeBriques[i][0]-1)*longBrique + ' / Y = ' + (listeBriques[i][1]-1)*hautBrique);
+			x = (listeBriques[i][1]-1)*longBrique;
+			y = (listeBriques[i][0]-1)*hautBrique;
+			ctx.fillStyle = (listeBriques[i][2]);
+			ctx.fillRect(x,y,longBrique,hautBrique);
 		}
 	}
 	
