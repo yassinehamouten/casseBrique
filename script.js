a96c7a2a4e88377083ea4019e59cad578ed40f67
Gilles <gilux762@gmail.com>
2016-03-31 09:01:59 +0200

    Création objets balle et brique + variables

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
 $(document).on('ready',function(){
-  
+	
+	var longBrique = 50; // Longueur d'une brique
+	var hautBrique = 30; // Hauteur d'une brique
+	
+	var listeBriques = [
+	[1,1],
+	[1,2],
+	[1,4],
+	[1,5],
+	[2,1]
+	];
+	
+	drawLevel();
+
+	var balle = function(x,y)
+	{
+		this.x = x;
+		this.y = y;
+		this.radius = 10;
+		this.couleur = "#000000";
+		this.vitesseX = 0;
+		this.vitesseY = 0;
+	}
+
+	var brique = function(x,y,color,pouvoir)
+	{
+		this.x = x;
+		this.y = y;
+		this.lon = 50;
+		this.lar = 30;
+		this.couleur = color;
+		this.pouvoir = null;
+	}
+
+	function drawLevel()
+	{
+		for(i in listeBriques)
+		{
+			console.log('x = ' + (listeBriques[i][0]-1)*longBrique + ' / Y = ' + (listeBriques[i][1]-1)*hautBrique);
+		}
+	}
 	
 }); 
